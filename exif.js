c6fbb30336f279c16fde34b75ba801cce7027391
muzaffer tas <muzaffertasiran@gmail.com>
2024-01-15 17:40:54 +0300

    bug fix

--- exif.js
+++ exif.js
@@ -738,7 +738,8 @@
 
     function getStringFromDB(buffer, start, length) {
         var outstr = "";
-        for (var n = start; n < start+length; n++) {
+        var n = "";
+        for (n = start; n < start+length; n++) {
             outstr += String.fromCharCode(buffer.getUint8(n));
         }
         return outstr;
